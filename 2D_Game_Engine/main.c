e6008d3059069e52b23bc8100386f3d44ef24789
hunterad96 <hunteradonald@gmail.com>
2023-09-19 21:23:54 -0500

    first commit

---
+++ 2D_Game_Engine/main.c
@@ -0,0 +1,22 @@
+#include "game.h"
+
+Game *game = nullptr;
+
+int main(int argc, char* argv[])
+{
+    game = new Game();
+
+    game->init("Dookie Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
+
+    while (game->running())
+    {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+
+    game->clean();
+
+    return 0;
+}
+
