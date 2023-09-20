e5f9277d14bc4550da8e018221fd37f5ef917058
hunterad96 <hunteradonald@gmail.com>
2023-09-19 20:33:39 -0500

    Initial Commit

---
+++ 2D_Game_Engine/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  2D_Game_Engine
+//
+//  Created by Hunter Donald on 9/19/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ 2D_Game_Engine/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  2D_Game_Engine
+//
+//  Created by Hunter Donald on 9/19/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
